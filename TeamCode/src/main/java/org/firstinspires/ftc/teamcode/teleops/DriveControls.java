a2eec4857a140992649d716c4d56f5207e7f13fe
Kevdog5000 <24kou@brentwoodmoschools.org>
2022-11-15 16:20:14 -0600

    feat: extender and claw implemented

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/DriveControls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/DriveControls.java
@@ -4,15 +4,17 @@ import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.teamcode.core.Poser;
-import org.opencv.core.Mat;
 
 @TeleOp(group = "drive")
 @Config
@@ -21,14 +23,15 @@ public class DriveControls extends LinearOpMode {
   BNO055IMU.Parameters imuParameters = new BNO055IMU.Parameters();
 
   BNO055IMU imu;
-  DcMotorEx leftFront, leftRear, rightRear, rightFront;
+  DcMotorEx leftFront, leftRear, rightRear, rightFront, extender;
+  Servo claw;
 
   Orientation orientation;
   double gamepadMoveX;
   double gamepadMoveY;
   double gamepadMove;
   double gamepadAngle;
-  double initialRobotAngle; // TODO: set via dpad controls
+  double initialRobotAngle;
   double robotAngle;
   double gamepadTurnY;
   double movementAngle;
@@ -40,6 +43,16 @@ public class DriveControls extends LinearOpMode {
   public static double ROTATION_SCALE = 0.75;
   public static double ROTATION_DAMPENING = 2;
 
+  public static double CLAW_CLOSED = 1;
+  public static double CLAW_OPENED = 0.75;
+
+  public static double EXTENDER_POWER = 0.5;
+  public static int EXTENDER_MIN = 0;
+  public static int EXTENDER_MAX = 3200;
+  public static int EXTENDER_SENSITIVITY = 20;
+  int extenderOffset;
+  int extenderState = EXTENDER_MIN;
+
   @Override
   public void runOpMode() throws InterruptedException {
     imuParameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
@@ -48,13 +61,28 @@ public class DriveControls extends LinearOpMode {
     leftRear = hardwareMap.get(DcMotorEx.class, "leftRear");
     rightRear = hardwareMap.get(DcMotorEx.class, "rightRear");
     rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+    extender = hardwareMap.get(DcMotorEx.class, "extender");
+    claw = hardwareMap.get(Servo.class, "claw");
 
     imu.initialize(imuParameters);
+
     leftRear.setDirection(DcMotorSimple.Direction.REVERSE);
     rightRear.setDirection(DcMotorSimple.Direction.REVERSE);
     rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+    leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    leftRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    rightRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+    extenderOffset = extender.getCurrentPosition();
+    extender.setTargetPositionTolerance(200);
+    extender.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    extender.setTargetPosition(-extenderState + extenderOffset);
+    extender.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    extender.setPower(EXTENDER_POWER);
 
     waitForStart();
+
 
     while (!isStopRequested()) {
       gamepadMoveX = poser.dampen(-gamepad1.left_stick_x, POSITION_DAMPENING, POSITION_SCALE);
@@ -87,6 +115,25 @@ public class DriveControls extends LinearOpMode {
           initialRobotAngle = Math.PI / 2;
         }
       }
+
+      claw.setPosition(gamepad2.right_bumper ? CLAW_OPENED : CLAW_CLOSED);
+
+      extenderState = (int) Math.min(
+        EXTENDER_MAX,
+        Math.max(
+          EXTENDER_MIN,
+          extenderState
+            + EXTENDER_SENSITIVITY * gamepad2.right_trigger
+            - EXTENDER_SENSITIVITY * gamepad2.left_trigger
+        )
+      );
+      extender.setTargetPosition(-extenderState + extenderOffset);
+
+      telemetry.addData("state", extenderState);
+      telemetry.addData("calculated state", -extenderState);
+      telemetry.addData("current pos", extender.getCurrentPosition());
+      telemetry.addData("current target", extender.getTargetPosition());
+      telemetry.update();
     }
   }
 }
